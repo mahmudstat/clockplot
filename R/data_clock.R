dfee8b060210aa758e9ffe0565b6bf950e9ed2ae
mahmud <almahmud.sbi@gmail.com>
2022-09-13 13:24:35 +0600

    Try to apply data_clock function to external data

--- R/data_clock.R
+++ R/data_clock.R
@@ -1,6 +1,6 @@
 #' @export
 
-data_clock <- function(Data, Time){
+data_clock <- function(Data, Time, Criteria){
   # data preparation
   data <- Data %>% mutate(time = Time/100) %>%
     separate(time, into = c("hour", "minute")) %>%
@@ -15,20 +15,20 @@ data_clock <- function(Data, Time){
            y1 = sin(time_angle)*.9,
            x0 = rep(0, dim(Data)[1]),
            y0 = rep(0, dim(Data)[1]))
-  
+
   ## Clock preparation
-  
+
   ampm = c(rep(" AM",6), rep(" PM",12), rep(" AM",6))
   k <- 24
   times <- exp(1i * 2 * pi * (k:1) / k)
   dfclock <- tibble(time = times,
                     label = paste0(c(6:12, 1:5), ampm))
   # Plot
-  
+
   data %>% ggplot(aes(x0,y0))+
     geom_segment(aes(xend = x1, yend = y1,
-                     color = timefreq))+
-    scale_color_gradient("timefreq",
+                     color = Criteria))+
+    scale_color_gradient("Criteria",
                          low = "green",
                          high = "red")+
     geom_text(data = dfclock,
