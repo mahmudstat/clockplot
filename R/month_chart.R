fbfbb66106414f523675ee95318d8af0f8a670a1
mahmudstat <almahmud.sbi@gmail.com>
2024-08-25 21:36:44 +0600

    add month chart and modify year chart

--- R/month_chart.R
+++ R/month_chart.R
@@ -2,31 +2,34 @@
 #'
 #' This function plots values corresponding to each month on a rose plot.
 #'
+#' @param df A data frame containing days of the month and corresponding values.
+#' @param Days Days of the month; should start from 1 and be between 28 and 31
 #' @param mvalue A numeric vector having values in each month of the year (starts
 #' from January, obviously)
 #' @param lgnm Title of legend. The legend position can also be changed by
-#' adding `legend.position = "top"`, where `R` accepts as positions
+#' adding `legend.position = "POSITION"`, where the accepted positions are
 #' `top`, `bottom`, `left`, and `right`.
+#' @param width Width of bars
 #' @param high The color name for the high values. The default is `red`
 #' @param low The color name for the high values. The default is `green`.
 #' #' The color names can be vice versa or other colors, depending on the context.
-#' @param width Width of bars
 #' @return A circular chart showing values in each month of the year.
 #' @name month_chart
 NULL
 #' @examples
-#' syltmp <- c(18.4, 20.8, 24.3, 26.0, 26.8, 27.6, 28.0, 28.2, 27.9, 26.7, 23.3, 19.7)
-#' month_chart(mvalue = syltmp)
+#' Days <- 1:30
+#' expense <- c(sample(15:20, 15, replace = TRUE),
+#'              sample(10:15, 5, replace = TRUE),
+#'              sample(8:10, 5, replace = TRUE),
+#'              sample(5:8, 3, replace = TRUE),
+#'              sample(4:7, 2, replace = TRUE))
+#' dfm <- tibble::tibble(Days, expense)
+#' month_chart(df = dfm, Days = Days, mvalue = expense)
 #' @export
-month_chart <- function(mvalue, lgnm = "Value", width = 0.9,
+month_chart <- function(df, Days, mvalue, lgnm = "Value", width = 0.9,
                         high = "yellow", low = "green"){
-  Months <- c("January", "February", "March", "April", "May", "June",
-              "July", "August", "September", "October", "November", "December")
-  Months <- keep_fct_order(Months)
-  # mvalue is a vector. If you have it in a data frame, use dataframe$mvalue
-  df <- tibble::tibble(Months, mvalue) %>%
-    dplyr::mutate(days = factor(Months, levels = Months))
-  chart <- df %>% ggplot2::ggplot(ggplot2::aes(Months, mvalue, fill = mvalue))+
+  df <- df %>% dplyr::mutate(Days = factor(Days, levels = Days))
+  chart <- df %>% ggplot2::ggplot(ggplot2::aes(Days, mvalue, fill = mvalue))+
     ggplot2::geom_col(width = width)+
     ggplot2::scale_fill_gradient(high = {{high}}, low = {{low}}, name = {{lgnm}})+
     ggplot2::coord_polar("x", start = 270)+
@@ -40,3 +43,4 @@ month_chart <- function(mvalue, lgnm = "Value", width = 0.9,
   # + scale_fill_gradient(name = "NAME")
   return(chart)
 }
+
