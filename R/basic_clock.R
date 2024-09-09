9b2cfc0c389f77fb2fc20d506946f6f315faf27f
mahmudstat <almahmud.sbi@gmail.com>
2024-09-09 18:06:19 +0600

    change theme (add lines to all hours

--- R/basic_clock.R
+++ R/basic_clock.R
@@ -2,17 +2,26 @@
 
 basic_clock <- function(){
   k <- 24 # Hours
+  origin <- rep(0, 24)
   subk <- 24*4
   times <- exp(1i * 2 * pi * (k:1) / k)
   subtimes <- data.frame(SubT = exp(1i * 2 * pi * (subk:1) / subk))
   ampm = c(rep(" AM",6), rep(" PM",12), rep(" AM",6))
   dfclock <- tibble::tibble(time = times,
                             hour = c(6:12, 1:12, 1:5), # May not be needed
-                            label = paste0(c(6:12, 1:5), ampm))
+                            label = paste0(c(6:12, 1:5), ampm),
+                            origin = origin)
   clock_skeleton <-  ggplot2::ggplot(dfclock)+
     ggplot2::geom_text(data = dfclock, ggplot2::aes(Re(.data$time)*1.1,
                                                     Im(.data$time)*1.1,
                                                     label = .data$label))+
+    ggplot2::geom_segment(ggplot2::aes(x= .data$origin,
+                                       y = .data$origin,
+                                       xend = Re(.data$time),
+                                       yend = Im(.data$time)),
+                          color = "black",
+                          alpha = 0.4,
+                          linetype = "dotted")+
     ggplot2::geom_point(data = subtimes,
                         ggplot2::aes(Re(.data$SubT), Im(.data$SubT)),
                         shape = 19, color = "black", size = 0.6)+
@@ -28,6 +37,8 @@ basic_clock <- function(){
                    axis.text.y=ggplot2::element_blank(),
                    axis.ticks.y=ggplot2::element_blank(),
                    legend.position = "none",
+                   panel.grid.major = element_blank(), # Remove grid lines
+                   panel.grid.minor = element_blank(), # Remove grid lines
                    aspect.ratio = 1)#+
     #ggplot2::geom_point(ggplot2::aes(0,0), color = "black", size = 2)
   return(clock_skeleton)
