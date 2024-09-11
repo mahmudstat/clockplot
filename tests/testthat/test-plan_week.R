b2fddf5e1e6ace08b5d34ac8e95540f79a811323
mahmudstat <almahmud.sbi@gmail.com>
2024-09-11 06:57:54 +0600

    add expec_vector test

---
+++ tests/testthat/test-plan_week.R
@@ -0,0 +1,8 @@
+# Test whether input is a character
+
+test_that("A character vector", {
+  wt <- LETTERS[1:7]
+  plan_week(wt)
+  expect_vector(wt, ptype = character())
+})
+
--- tests/testthat/test-week_chart.R
+++ tests/testthat/test-week_chart.R
@@ -2,9 +2,14 @@
 # Using expect_length()
 
 test_that("Length of vector is 7", {
-  wt <- sample(10,7)
-  week_chart(wt)
-  expect_length(wt, 7)
+  wv <- sample(10,7)
+  week_chart(wv)
+  expect_length(wv, 7)
 })
 
+test_that("A numeric vector", {
+  wt <- sample(10,7)+runif(7)
+  week_chart(wt)
+  expect_vector(wt, ptype = numeric())
+})
 
