7a522df4cb2140f5e4d0ec1ded6bea857b439e96
mahmudstat <almahmud.sbi@gmail.com>
2024-09-13 00:08:51 +0600

    terminate conv_hms

--- R/clean_data.R
+++ R/clean_data.R
@@ -7,26 +7,26 @@
 #' The converted `HH:MM:SS` actually gives `HH:MM:00` format, the second part
 #' made `00`, since in practice it has negligible effect on the plot.
 #'
-#' @param df A data set
-#' @param time a `time` or `character` variable in the format `HH:MM`
-#' @param new New name of the variable
-#' @name conv_hms
+#' # @param df A data set
+#' # @param time a `time` or `character` variable in the format `HH:MM`
+#' # @param new New name of the variable
+#' # @name conv_hms
 
-#' @return Time in HH:MM format
-#' @examples
-#' df <- data.frame(time = c("06:00", "08:00", "17:30"))
-#' df = conv_hms(df, time, New)
-#' df
+#' # @return Time in HH:MM format
+#' # @examples
+#' # df <- data.frame(time = c("06:00", "08:00", "17:30"))
+#' # df = conv_hms(df, time, New)
+#' # df
 
 
-conv_hms <- function(df, time, new){
-  df <- mutate(df, new = hms::parse_hm({{time}}))
-  .Deprecated("hms::parse_hm")
-}
+# conv_hms <- function(df, time, new){
+ # df <- mutate(df, new = hms::parse_hm({{time}}))
+ # .Deprecated("hms::parse_hm")
+# }
 
 # Old function. Will be deprecated
-rm_sec <- function(df, time) {
-  df <- mutate(df, time = gsub('.{3}$', '', {{time}}))
-}
+#rm_sec <- function(df, time) {
+ # df <- mutate(df, time = gsub('.{3}$', '', {{time}}))
+# }
 
 
